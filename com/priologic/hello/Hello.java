6af23d30985ed5d349c12184be1c33d4c3377136
wfang-prio <42452258+wfang-prio@users.noreply.github.com>
2020-03-24 15:01:15 -0700

    Update Hello.java

--- com/priologic/hello/Hello.java
+++ com/priologic/hello/Hello.java
@@ -2,7 +2,15 @@ package com.priologic.hello;
 
 public class Hello {
 	public static void main(String[] args) {
-		System.out.println("Hello World!");
+		StringBuilder sb = new StringBuilder();
+		sb.append("Hello ");
+		if (args.length > 0) {
+			sb.append(args[0]);
+		} else {
+			sb.append("World");
+		}
+		sb.append("!");
+		System.out.println(sb.toString());
 		System.exit(0);
 	}
 }
